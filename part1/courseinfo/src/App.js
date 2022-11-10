62ca60ac046e03e2c873e45201da1a4ad8c12afd
Imran Dhahbi <imrandhahbi@192-168-1-109.tpgi.com.au>
2022-11-11 00:37:41 +1100

    ex 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -24,32 +24,37 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  const sum = props.sum.reduce((previousValue, currentValue) => previousValue + currentValue.exercises, 0)
   return (
-    <p>Number of exercises {props.sum}</p>
+    <p>Number of exercises {sum}</p>
   )
 }
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content props={[part1, part2, part3]} />
-      <Total sum={part1.exercises+part2.exercises+part3.exercises}/>
+      <Header course={course.name} />
+      <Content props={course.parts} />
+      <Total sum={course.parts}/>
 
     </div>
   )
