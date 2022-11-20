0ad73fe961f71eb6a4f988e5e2487af8e093f44c
Imran Dhahbi <imrandhahbi@192-168-1-109.tpgi.com.au>
2022-11-20 15:51:48 +1100

    added states

--- part1/dev/c_component_state/src/App.js
+++ part1/dev/c_component_state/src/App.js
@@ -1,26 +1,33 @@
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age
+import { useState } from 'react'
 
+const Display = ({counter}) => <div>{counter}</div>
 
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>so you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+  
 
 const App = () => {
-  const name = 'peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="maya" age={26+10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter}/>
+      <Button 
+        onClick={increaseByOne} 
+        text={'increment'}
+      />
+      <Button 
+        onClick={decreaseByOne} 
+        text={'decrement'} 
+      />
+
+      <Button 
+        onClick={setToZero} 
+        text={'reset'}
+      />
     </div>
   )
 }
